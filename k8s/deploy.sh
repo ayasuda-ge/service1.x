94590f7ba48ac1873906bfcae271d423d3f5ae98
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2022-07-05 13:33:37 -0700

    Update deploy.sh

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -95,14 +95,7 @@ function int_a() {
         case $ref1 in
          ZONE)
          
-          : 'if [[ "$ref2" == *"b472-40b8-83e2"* ]] || [[ "$ref2" == *"8577-4685-bf3c"* ]] || [[ "$ref2" == *"c556-4d31-869f"* ]] || [[ "$ref2" == *"7fe6-413c-8330"* ]] || [[ "$ref2" == *"b317-4aaa-a92b"* ]] || [[ "$ref2" == *"a751-4cf3-9caf"* ]] || [[ "$ref2" == *"de07-48c7-a191"* ]]; then
-            echo ""
-            echo " [!] service ${ref2} discarded."
-            echo ""
-            continue
-          fi'
-          
-          if [[ "$ref2" != *"8577-4685-bf3c"* ]]; then
+          if [[ "$ref2" == *"888d91ee-c556-4d31-869f-c6e937bfa7af"* ]] || [[ "$ref2" == *"9e768876-7fe6-413c-8330-9e2d7ad42c7b"* ]]; then
             echo ""
             echo " [!] service ${ref2} discarded."
             echo ""
